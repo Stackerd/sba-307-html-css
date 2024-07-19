d0b01992e40f5749c36ff28d1c5372fdbfc0c244
stackerd <danni2mar@gmail.com>
2024-07-19 16:54:14 -0400

    deleted title created h1 element in script file

--- sba-307-html-and-css/script.js
+++ sba-307-html-and-css/script.js
@@ -10,3 +10,7 @@ logIn.addEventListener('mouseover', function(){
 logIn.addEventListener('mouseout', function(){
     logIn.style.color = "green"
 });
+
+const h1 = document.createElement('h1');
+h1.innerText = 'Time to learn' 
+logIn.previousElementSibling.prepend(h1)
