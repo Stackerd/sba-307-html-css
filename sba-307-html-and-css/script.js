84d5d5669ce75e735b3b6d7c08a27bab6fb50211
stackerd <danni2mar@gmail.com>
2024-07-19 17:04:22 -0400

    created a new document fragment updated intertext based on user interaction

--- sba-307-html-and-css/script.js
+++ sba-307-html-and-css/script.js
@@ -14,3 +14,11 @@ logIn.addEventListener('mouseout', function(){
 const h1 = document.createElement('h1');
 h1.innerText = 'Time to learn' 
 logIn.previousElementSibling.prepend(h1)
+
+const fragment = new DocumentFragment();
+const h1clone = h1.cloneNode();
+
+fragment.appendChild(h1clone)
+h1.addEventListener('click', function() {
+    h1.innerText = 'ALREADY LEARNED'
+})
